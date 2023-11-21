eb6df1bdbb05fd6a702e5c93d731f85e4ec149b7
Ihor <108004180+beligrous@users.noreply.github.com>
2023-11-21 11:33:45 +0200

    add routing

---
+++ src/app/posts/page.tsx
@@ -0,0 +1,9 @@
+const Posts = () => {
+  return (
+    <>
+      <h1>Страница статей</h1>
+    </>
+  );
+};
+
+export default Posts;
---
+++ src/app/servise/page.tsx
@@ -0,0 +1,9 @@
+const Service = () => {
+  return (
+    <>
+      <h1>Страница услуг</h1>
+    </>
+  );
+};
+
+export default Service;
